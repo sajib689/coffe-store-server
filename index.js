43d0be0d446e3cbf85df31d5006d37b6be98461e
sajib689 <jibonraja11@gmail.com>
2023-09-13 17:34:17 +0600

    jwt add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
+var jwt = require('jsonwebtoken');
 const port = process.env.PORT || 3000;
 require('dotenv').config()
 //middleware
@@ -19,12 +20,28 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
+const verifyJWT = (req, res, next) => {
+    
+    const authorization = req.headers.authorization
+    console.log(authorization)
+    if(!authorization) {
+    return res.status(401).send({error: true, message: 'Unauthorized access denied'})
+    }
+    const token = authorization.split(' ')[1]
+    jwt.verify(token, process.env.JWT_SECRET,(error, decoded) =>{
+      if(error) {
+        return res.send({error: true, message: 'Unauthorized access denied'})
+      }
+      req.decoded = decoded
+      next()
+    })
+};
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const coffeesCollection = client.db('coffeeDb').collection('coffees')
+    const ordersCollection = client.db('coffeeDb').collection('orders')
     // send data to database api
     app.post('/coffees', async(req, res) => {
         const coffee = req.body
@@ -70,6 +87,38 @@ async function run() {
       }
       const result = await coffeesCollection.updateOne(query, updatedCoffee,options)
       res.send(result)
+    })
+    // send orders
+    app.post('/orders', async (req, res) => {
+      const query = req.body
+      const result = await ordersCollection.insertOne(query)
+      res.send(result)
+    })
+    // get orders from the database
+    app.get('/orders',verifyJWT, async (req, res) => {
+      // console.log(req.headers.authorization)
+      let query = {}
+      if(req.query?.email){
+        query={email: req.query.email} 
+      }
+      const result = await ordersCollection.find(query).toArray()
+    res.send(result)
+    })
+    // delete orders from the database
+    app.delete('/orders/:id', async (req, res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const orderDelete = await ordersCollection.deleteOne(query)
+      res.send(orderDelete)
+    })
+    // post jwt
+    app.post('/jwt', (req, res) => {
+      const user = req.body
+      console.log(user)
+      const token = jwt.sign(user, process.env.JWT_SECRET, {
+        expiresIn: '1h'
+      })
+      res.send({token})
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
